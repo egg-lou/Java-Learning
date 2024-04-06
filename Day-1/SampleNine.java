c8f1e469d483e329311420c1910a3ef5886c4cd1
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-06 09:40:06 +0800

    Sample Nine

---
+++ Day-1/SampleNine.java
@@ -0,0 +1,12 @@
+public class SampleNine {
+    public static void main(String []args) {
+        int num1 = 95;
+        char char1 = (char)num1;
+
+        int num2  = 65;
+        char char2 = (char)num2;
+
+        System.out.println("char1 : " + char1);
+        System.out.println("char 2 : " + char2);
+    }
+}
