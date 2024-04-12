3f19173cecedb05084dd8159fd4795015f855980
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-12 09:25:22 +0800

    Add Files - Day 3

---
+++ Day-3/SampleSix.java
@@ -0,0 +1,13 @@
+public class SampleSix {
+    public static void main(String[] args) {
+       String str = "Java Training 2024";
+        int i = 4;
+
+        if (i == 4) {
+            i++;
+            System.out.println(str);
+        }
+
+        System.out.println("i = " + i);
+    }
+}
