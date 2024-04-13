eff217292b113fb6304168e18a0ba129a9b391b6
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-13 08:47:40 +0800

    Sample Seven - Day -4

---
+++ Day-4/SampleSeven.java
@@ -0,0 +1,12 @@
+public class SampleSeven {
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 50;
+
+        if (x == 30) {
+            if (y == 50) {
+                System.out.println("Correct Answer");
+            }
+        }
+    }
+}
