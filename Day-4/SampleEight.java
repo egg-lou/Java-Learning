1a60ed62d4463fa317b0340fce7836e6b539cfe8
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-13 09:42:28 +0800

    Sample Eight - Day -4

---
+++ Day-4/SampleEight.java
@@ -0,0 +1,17 @@
+public class SampleEight {
+    public static void main(String[] args) {
+        int age = 25;
+        int weight = 60;
+
+
+        if (age >= 18) {
+            if (weight > 50) {
+                System.out.println("You are eligible to donate blood");
+            } else {
+                System.out.println("You are not eligible to donaten blood");
+            }
+        } else {
+            System.out.println("Age must be greater than 18");
+        }
+    }
+}
