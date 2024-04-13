cc72ebc53a624f3ebe2597ab30c3f3597fc90867
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-13 08:39:20 +0800

    Sample Six - Day -4

---
+++ Day-4/SampleSix.java
@@ -0,0 +1,12 @@
+public class SampleSix {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        if (a == 10) {
+            if (b == 20) {
+                System.out.println("Welcome to Java Training");
+            }
+        }
+    }
+}
