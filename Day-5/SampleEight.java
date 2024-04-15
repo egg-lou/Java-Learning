d58495e5ddc62c852c78cf16b207a2caa9107ab8
egg-lou <miguel.rafaellouie2003@gmail.com>
2024-04-15 09:01:54 +0800

    Day-5 add tasks

---
+++ Day-5/SampleEight.java
@@ -0,0 +1,11 @@
+public class SampleEight {
+ public static void main(String[] args) {
+    int x = 10, sum = 10;
+
+    do {
+        sum += x;
+        x--;
+    } while (x > 10);
+    System.out.println("Summation: " + sum);
+    }   
+}
