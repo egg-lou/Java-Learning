27e0f7122344b9ac8f90468284d6fba4e5072273
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-19 08:48:30 +0800

    add files day-7

---
+++ Day-7/SampleFive.java
@@ -0,0 +1,11 @@
+public class SampleFive {
+  public static void main(String[] args) {
+    String s1 = "My name is axcle. My name is MATTHEW JERICHO. My name is SHANEN AUDREY";
+
+
+    String replaceString = s1.replaceAll("\\s", "");
+
+    System.out.println(replaceString);
+    
+  }
+}
