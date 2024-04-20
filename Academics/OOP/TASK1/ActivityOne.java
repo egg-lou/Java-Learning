047fb10c200f83c2ca799340650b679cb86a6b00
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-20 20:23:44 +0800

    add academics task

---
+++ Academics/OOP/TASK1/ActivityOne.java
@@ -0,0 +1,15 @@
+package Academics.OOP.TASK1;
+
+public class ActivityOne {
+    public static void main(String[] args) {
+        String firstName = "Alenere";
+        String secondName = "David";
+        String gender = "she";
+        String drink = "coffee";
+
+        String message = firstName + ", the friendly neighbor, waved at " + secondName + " as " + gender + " walked by " + secondName + "'s house. " + secondName + " smiled back and invited " + firstName + " in for a cup of " + drink;
+
+        System.out.println(message);
+    }
+    
+}
