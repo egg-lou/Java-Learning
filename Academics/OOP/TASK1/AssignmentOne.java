b80b1a9109346bc73df980a2670e19619ef7db82
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-21 20:10:23 +0800

    add files

--- Academics/OOP/TASK1/AssignmentOne.java
+++ Academics/OOP/TASK1/AssignmentOne.java
@@ -28,33 +28,15 @@ public class AssignmentOne {
         System.out.println(winner);
     }
 
-    // private static String determineWinner(Choice playerOneChoice, Choice playerTwoChoice) {
-    //     if(playerOneChoice == playerTwoChoice) {
-    //         return "It's a tie!";
-    //     } else if((playerOneChoice == Choice.ROCK && playerTwoChoice == Choice.SCISSORS) ||
-    //               (playerOneChoice == Choice.SCISSORS && playerTwoChoice == Choice.PAPER) ||
-    //               (playerOneChoice == Choice.PAPER && playerTwoChoice == Choice.ROCK)) {
-    //         return "Player 1 wins!";
-    //     } else {
-    //         return "Player 2 wins!";
-    //     }   
-    // }
-
     private static String determineWinner(Choice playerOneChoice, Choice playerTwoChoice) {
-        
-        if (playerOneChoice == playerTwoChoice) {
+        if(playerOneChoice == playerTwoChoice) {
             return "It's a tie!";
-        }
-
-        Map<Choice, Choice> winningConditions = new HashMap<>();
-        winningConditions.put(Choice.ROCK, Choice.SCISSORS);
-        winningConditions.put(Choice.SCISSORS, Choice.PAPER);
-        winningConditions.put(Choice.PAPER, Choice.ROCK);
-
-        if (playerOneChoice == winningConditions.get(playerTwoChoice)) {
-            return "Winner: Player 1!";
+        } else if((playerOneChoice == Choice.ROCK && playerTwoChoice == Choice.SCISSORS) ||
+                  (playerOneChoice == Choice.SCISSORS && playerTwoChoice == Choice.PAPER) ||
+                  (playerOneChoice == Choice.PAPER && playerTwoChoice == Choice.ROCK)) {
+            return "Player 1 wins!";
         } else {
-            return "Winner: Player 2!";
-        }
+            return "Player 2 wins!";
+        }   
     }
 }
