7d01fbc1ff98747e427f917da6c6763a8eb5b102
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-22 10:02:00 +0800

    init day-8

---
+++ Day-8/ActivityOne.java
@@ -0,0 +1,8 @@
+public class ActivityOne {
+    public static void main(String[] args) {
+        String name = "Joysis Java Training";
+
+        char ch = name.charAt(8);
+        System.out.println("Character at 8th index: " + ch);
+    }    
+}
