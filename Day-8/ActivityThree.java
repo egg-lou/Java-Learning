76371f45f01fd6fb5b9932ddecfb9408fd5af1f4
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-22 10:05:21 +0800

    update act 3 day-8

--- Day-8/ActivityThree.java
+++ Day-8/ActivityThree.java
@@ -1,6 +1,11 @@
 
 public class ActivityThree {
     public static void main(String[] args) {
-        
+        String str1 =  "Learn Java";
+        String str2 = "Learn\nJava";
+
+        System.out.println(str1.charAt(0));
+        System.out.println(str1.charAt(6));
+        System.out.println(str2.charAt(5));
     }    
 }
