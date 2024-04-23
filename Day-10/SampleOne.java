059236ed04e1f7e637e32fb6471de1b9e3e8f2cb
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-24 07:27:57 +0800

    init Day-10

---
+++ Day-10/SampleOne.java
@@ -0,0 +1,11 @@
+public class SampleOne {
+    public static void main(String[] args) {
+        String s = " geeks for geeks has all java functions to read ";
+
+        System.out.println(s.trim());
+
+        s = " Chetna loves to read books ";
+
+        System.out.println(s.trim());
+    }
+}
--- Day-4/SampleTwo.java
+++ Day-4/SampleTwo.java
@@ -1,7 +1,5 @@
 public class SampleTwo {
     public static void main(String[] args) {
-        int marks = 0;
-
 
     }
 }
