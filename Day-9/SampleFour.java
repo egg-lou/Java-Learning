f58b738773759d33f74bdb610e0c05033965b018
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-23 09:13:00 +0800

    wala lang

---
+++ Day-9/SampleFour.java
@@ -0,0 +1,13 @@
+public class SampleFour {
+public static void main(String[] args) {
+    String str1 = "Learn Java"; // string java 10
+    String str2 = "Learn Kotlin"; // string kotlin 12
+    
+    int result;
+
+    // comparing str1 with str2
+    result = str1.compareTo(str2);
+
+    System.out.println(result);
+}   
+}
---
+++ Day-9/SampleThree.java
@@ -0,0 +1,9 @@
+public class SampleThree {
+    public static void main(String[] args) {
+        
+         String gfg = new String("Welcome to Joysis Training");
+        System.out.print("Found g after 24th index at position: ");
+ 
+        System.out.println(gfg.indexOf('g', 13));
+    }
+}
