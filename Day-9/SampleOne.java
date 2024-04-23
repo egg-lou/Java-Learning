d8181d5a308c77d085de7cb17bedc234c0a9d1b0
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-23 08:57:27 +0800

    add files - Day -9

--- Day-9/SampleOne.java
+++ Day-9/SampleOne.java
@@ -1,6 +1,11 @@
 public class SampleOne {
   public static void main(String[] args) {
+    String s1 = "This is indexOf method";
 
+    int index = s1.indexOf("method");
+    
+
+    System.out.println("index of substring: " + index);
     
   }
 }
---
+++ Day-9/SampleTwo.java
@@ -0,0 +1,9 @@
+public class SampleTwo {
+    public static void main(String[] args) {
+        String gfg = "Welcome to Joysis Java Training"; 
+ 
+        System.out.print("Found g first at position : ");
+ 
+        System.out.println(gfg.indexOf('g'));
+    }
+}
