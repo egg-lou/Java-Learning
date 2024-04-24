13546865e0d876bd6557007f5eba9db3ab9a6139
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-24 08:41:58 +0800

    add file day 10

--- Day-10/SampleEight.java
+++ Day-10/SampleEight.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 public class SampleEight {
     public static void main(String[] args) {
+        // Custom input string
+        String s = "I Know YOI Bui You Don't Know ME"; // string variable value
+ 
+        // Locales with the language "tr" for TURKISH
+        //"en" for ENGLISH is created
+        Locale TURKISH = Locale.forLanguageTag("tr"); // locale tur tr
+        Locale ENGLISH = Locale.forLanguageTag("en");// locale en
+ 
+        // Converting string s to lowercase letter
+        // using TURKISH and ENGLISH language
+        String gfg2 = s.toLowerCase(TURKISH);// lower case
         
+        String gfg3 = s.toLowerCase(ENGLISH); // lower case
+ 
+        // Printing above string
+        System.out.println(gfg2); // code to be exe.
+        
+        System.out.println(gfg3); // 
     }    
 }
