17e22591d81736fa8126364c74f43e674c121749
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-24 08:58:38 +0800

    add file day 10

---
+++ Day-10/SampleTen.java
@@ -0,0 +1,8 @@
+public class SampleTen {
+    public static void main(String[] args) {
+        String s1 = "hello charisse welcome to joysis java training";
+
+        String s1Upper = s1.toUpperCase();
+        System.out.println(s1Upper);
+    }    
+}
