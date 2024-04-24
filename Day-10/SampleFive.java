f7131a8adbda60392151569cadfb5ce8e7699bd7
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-24 08:18:52 +0800

    add file day 10

---
+++ Day-10/SampleFive.java
@@ -0,0 +1,10 @@
+public class SampleFive {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "Training";
+        
+        String str = String.format("JOYSIS TVI is: %1$s, %1$s and %2$s", str1, str2);
+        
+        System.out.println(str);
+    }
+}
---
+++ Day-10/SampleSix.java
@@ -0,0 +1,5 @@
+public class SampleSix {
+    public static void main(String[] args) {
+        
+    }    
+}
