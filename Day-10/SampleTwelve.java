e4b59248da0a87851a180e13767772481fa206f3
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-24 09:19:04 +0800

    add file day 10

---
+++ Day-10/SampleTwelve.java
@@ -0,0 +1,7 @@
+public class SampleTwelve {
+    public static void main(String[] args) {
+        String str = "Welcome! to JAVA TRAINING";
+        String strup = str.toUpperCase();
+        System.out.println(strup);
+    }    
+}
