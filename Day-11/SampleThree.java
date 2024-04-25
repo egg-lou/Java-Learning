5ddd6438c5535a2ebc8a702e3a695b93031e7c11
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-25 08:00:17 +0800

    new file day 11

---
+++ Day-11/SampleThree.java
@@ -0,0 +1,13 @@
+public class SampleThree {
+    public static void main(String[] args) {
+        String s = new String ("Joysis Java Training");
+        
+        char array[] = s.toCharArray();
+
+        System.out.print("The String i: ");
+
+        System.out.println(s.toCharArray());
+
+        System.out.println("Size of char is: " + array.length);
+    }
+}
