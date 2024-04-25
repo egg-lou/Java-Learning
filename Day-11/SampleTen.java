b82cbeb763617efb340d1a939a8571ea6a97c7b8
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-25 09:25:54 +0800

    new file day 11

---
+++ Day-11/SampleTen.java
@@ -0,0 +1,21 @@
+public class SampleTen {
+    public static void main(String[] args) {
+        int a[]=new int[5];//declaration and instantiation  // int data type a variable int [5]
+
+        a[0]=10;//initialization   10
+
+        a[1]=20;  // 20
+
+        a[2]=70;  //70
+
+        a[3]=40;  // 40
+
+        a[4]=50;  // 50
+
+        //traversing array  
+
+        for(int i=0;i<a.length;i++)//length is the property of array  for statement int data type i variable value o
+            // < operator a length i ++ inc
+        System.out.println(a[i]);  // code to be exe. 10 20 70 40 50
+    }
+}
