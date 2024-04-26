74369926d01c1b88361140a01b4f7bc0077d9b8e
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-26 08:42:55 +0800

    add SampleEight day 12

---
+++ Day-12/SampleEight.java
@@ -0,0 +1,26 @@
+import java.util.*; // import util
+
+public class SampleEight {// main class
+    
+    
+ public static void main(String args[]){  // main methods
+     
+  ArrayList<String> list = new ArrayList<String>();//Creating arraylist  
+  
+      list.add("Mango");//Adding object in arraylist    
+      
+      list.add("Apple");    
+      
+      list.add("Banana");    
+      
+      list.add("Grapes"); 
+      
+      list.add("cocunut"); 
+      
+      list.add("dragon fruit"); 
+      
+      //Printing the arraylist object 
+      
+      System.out.println(list);  // code to be exe. 
+ }  
+}
