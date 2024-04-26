c87594c83b2b23de977ea0aede0d005cb1accb72
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-26 09:00:49 +0800

    add SampleTen day 12

---
+++ Day-12/SampleTen.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList; // arraylist
+
+public class SampleTen { // main class
+
+  public static void main(String[] args){ // main methods
+
+    // create ArrayList
+    ArrayList<String> languages = new ArrayList<>(); // arraylist
+
+    // Add elements to ArrayList
+    languages.add("Java"); // java
+    
+    languages.add("Python");// python
+    
+    languages.add("Swift");// swift
+    
+    System.out.println("ArrayList: " + languages); // 
+  }
+}
