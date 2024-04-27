ee3da4e8622eb4e814bf51344dbdd2f832a39dfc
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-27 08:53:39 +0800

    sampleeight day13

---
+++ Day-13/SampleEight.java
@@ -0,0 +1,15 @@
+import java.time.OffsetTime; 
+
+public class SampleEight { // main class
+    
+     
+ 
+  public static void main(String[] args) {  //main methods
+      
+    OffsetTime offset = OffsetTime.now();  //offsettime
+    
+    int m = offset.getMinute();  //data type int variable m offset
+    
+    System.out.println(m+ " minute");  
+  }  
+}
