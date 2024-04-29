bf36aa0b57af06e326471a63c3fcb3645333b948
egglou <miguel.rafaellouie2003@gmail.com>
2024-04-29 09:39:22 +0800

    SampleFive day 14

--- Day-14/SampleFive.java
+++ Day-14/SampleFive.java
@@ -2,7 +2,7 @@ import java.time.OffsetDateTime;
 
 public class SampleFive {
     public static void main(String[] args) {
-         OffsetDateTime offsetDT = OffsetDateTime.now();
+         OffsetDateTime offsetDT = OffsetDateTime.now();    
        
         System.out.println(offsetDT.getDayOfWeek()); 
     }
