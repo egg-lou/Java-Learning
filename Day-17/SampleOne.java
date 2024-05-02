53ea3b1d22e900f65c7269c3ce6153b2d747f522
egglou <miguel.rafaellouie2003@gmail.com>
2024-05-03 07:32:57 +0800

    sample one day 17

--- Day-17/SampleOne.java
+++ Day-17/SampleOne.java
@@ -1,5 +1,36 @@
+class E {
+    public void print_A() { System.out.println("Manila Zoo"); }
+}
+
+class F extends E {
+    public void print_B() { System.out.println("Malabon Zoo"); }
+}
+
+class G extends E {
+    public void print_C() { System.out.println("Avilon Zoo"); }
+}
+
+class H extends E {
+    public void print_D() { System.out.println("Baluarte"); }
+}
+
+// Driver Class
+
+
 public class SampleOne {
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args)
+    {
+        //F obj_B = new F();
+        //obj_B.print_A();
+        //obj_B.print_B();
+
+        G obj_C = new G();
+        obj_C.print_A();
+        obj_C.print_C();
+
+        H obj_D = new H();
+        obj_D.print_A();
+        obj_D.print_D();
     }
 }
